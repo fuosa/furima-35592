77febccb503e0db34e65d36c7d9cb589569eac1f
fuosa <tokyo18osamu@yahoo.co.jp>
2021-05-14 11:17:10 +0900

    ログイン有無で処理を変える

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # validates :nickname, presence: true
+  # validates :email, presence: true
+  # validates :email, uniqueness: true
+  # validates :email, inclusion: { in: %w(@) }
+  # # validates :password, presence: true
+  # # validates :password, length: { minimum: 6 }
+  # # validates :password, format: { with: /\A[a-z0-9]+\z/i }
+  # # # validates :password_confirmation, presence: true
+  # # # validates :password, confirmation: true
+
 end
