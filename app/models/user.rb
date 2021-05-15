e6e1d261cc1b46f3921245dd0725575ec1c78cdf
fuosa <tokyo18osamu@yahoo.co.jp>
2021-05-16 00:19:50 +0900

    ユーザー管理機能修正_1

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :password_confirmation
+    validates :family_name
+    validates :first_name
+    validates :family_name_kana
+    validates :first_name_kana
+    validates :date_of_birth
+  end
+
+  with_options format: {with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ } do
+    validates :family_name
+    validates :first_name
+  end
+
+  with_options format: { with: /\A[ァ-ヶー－]+\z/} do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
   
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-  validates :password_confirmation, presence: true
-
-  validates :family_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
-
-  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
-
-  validates :date_of_birth, presence: true
 
 end
