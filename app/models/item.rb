3b15f3df64a4b51b4cd5a5859c82eafd370a1ca0
fuosa <tokyo18osamu@yahoo.co.jp>
2021-05-17 19:45:51 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :delivery_fee_id
+  belongs_to :category_id
+  belongs_to :condition_id 
+  belongs_to :prefecture_id 
+  belongs_to :shipping_day_id
+
+  validates :delivery_fee_id, :category_id, :condition_id, :prefecture_id, :shipping_day_id, presence: true, numericality: { other_than: 1 } 
 end
