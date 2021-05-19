dc02c053a68664b3fdbdc0ff422597f79d056389
fuosa <tokyo18osamu@yahoo.co.jp>
2021-05-19 11:20:01 +0900

    商品出品機能修正_2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'item_infos/new'
+  # 実装とは関係ないので一旦コメントアウト
+  # get 'item_infos/new'
+  # 実装とは関係ないので一旦コメントアウト
+  
   devise_for :users
   root to: "items#index"
 
