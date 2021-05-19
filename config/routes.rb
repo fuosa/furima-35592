5ea409e8e29418b89ce1bf15b48e704ef42f42d5
fuosa <tokyo18osamu@yahoo.co.jp>
2021-05-19 15:10:24 +0900

    Revert "Revert "Merge pull request #3 from fuosa/商品出品機能""
    This reverts commit 3b42d42f725409d7ea312f86a9ff92086ca83b3f.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # 実装とは関係ないので一旦コメントアウト
+  # get 'item_infos/new'
+  # 実装とは関係ないので一旦コメントアウト
+  
   devise_for :users
   root to: "items#index"
+
+  resources :items, only: [:index, :new, :create]
 end
