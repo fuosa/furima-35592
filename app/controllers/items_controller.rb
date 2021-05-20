cb43214a8ff56a9a7e9db9f79adc73d1d3639a91
fuosa <tokyo18osamu@yahoo.co.jp>
2021-05-20 17:28:20 +0900

    リダイレクト処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only:[:new, :create]
+  before_action :authenticate_user!, only:[:new, :create, :edit]
 
   def index
     @item = Item.all.order(created_at: :desc)
@@ -24,11 +24,29 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-  end 
+      unless @item.user_id == current_user.id
+        redirect_to action: :index
+      end
+    end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
 
   private
 
   def item_params
     params.require(:item).permit(:image, :item_name, :description, :price, :delivery_fee_id, :category_id, :condition_id, :prefecture_id, :shipping_day_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+   unless @item.user_id_signed_in?
+    redirect_to action: :index
+   end
   end
 end
