7bd9b2271c122a9ccb31524debddeef6c3e4d62c
fuosa <tokyo18osamu@yahoo.co.jp>
2021-05-20 18:28:18 +0900

    商品情報編集機能修正_2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only:[:new, :create, :edit, :update]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
 
   def index
@@ -29,10 +29,8 @@ class ItemsController < ApplicationController
     end
 
   def update
-    if @item.update(item_params)
+      unless @item.user_id == current_user.id
       redirect_to item_path
-    else
-      render :edit
     end
   end
 
