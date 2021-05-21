e62183cc7d13229ab79edd8b6ec9af7164aa6446
fuosa <tokyo18osamu@yahoo.co.jp>
2021-05-21 17:06:35 +0900

    Revert "formオブジェクト1"
    This reverts commit 8f9c98c5ab05973f2c39c90a55ae982a1d0b0c9a.

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,13 +20,17 @@ class Item < ApplicationRecord
     validates :delivery_fee_id
     validates :prefecture_id
     validates :shipping_day_id
+    validates :price
   end
   
   with_options numericality: { other_than: 0 } do
     validates :category_id
     validates :condition_id
     validates :delivery_fee_id
+    validates :prefecture_id
     validates :shipping_day_id
   end
+
+  validates :price, numericality: { only_integer: true, greater_than: 300, less_than: 10000000 }
 
 end
