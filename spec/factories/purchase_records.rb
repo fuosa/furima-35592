7795d5d6c108a9b3874611c694d5b9778c0182c6
fuosa <tokyo18osamu@yahoo.co.jp>
2021-05-24 11:18:55 +0900

    factoryBot記入

--- spec/factories/purchase_records.rb
+++ spec/factories/purchase_records.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :purchase_record do
+    # validates :post_code, format: { with: /\A\d{3}[-]\d{3}\z/}
+    # validates :prefecture_id, numericality: { other_than: 0 }
+    # validates :city
+    # validates :house_number
+    # validates :phone, format: { with: /\A[0-9]{11}\z/}
     
+    post_code           {123-4567}
+    prefecture_id       {2}
+    city                {横浜市}
+    house_number        {1-23-45}
+    phone               {09012345678}
   end
 end
