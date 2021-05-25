b4b5fbff8d69cdba867bc2cfd8829cd354431f31
fuosa <tokyo18osamu@yahoo.co.jp>
2021-05-25 11:13:46 +0900

    鍵情報の環境変数設定

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_afd5b8955519534b8964290f"); // PAY.JPテスト公開鍵
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
