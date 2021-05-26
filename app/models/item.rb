3aece7bcecc0808735077fbfac36b44868e350dd
fuosa <tokyo18osamu@yahoo.co.jp>
2021-05-26 18:05:56 +0900

    挙動確認修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,6 +20,7 @@ class Item < ApplicationRecord
     validates :condition_id
     validates :delivery_fee_id
     validates :shipping_day_id
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
   
   with_options numericality: { other_than: 0 } do
@@ -27,5 +28,6 @@ class Item < ApplicationRecord
     validates :condition_id
     validates :delivery_fee_id
     validates :shipping_day_id
+    validates :prefecture_id
   end
 end
